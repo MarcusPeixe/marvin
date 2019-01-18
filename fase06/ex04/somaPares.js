dc2e6e2ef8a2027c5959400e74bf8f7d257d6a3a
MarcusPeixe <46487114+MarcusPeixe@users.noreply.github.com>
2019-01-18 11:01:07 -0200

    Update somaPares.js

--- fase06/ex04/somaPares.js
+++ fase06/ex04/somaPares.js
@@ -1,6 +1,6 @@
 function somaPares(min, max) {
     var sum = 0;
-    for (var i = Math.floor(min); i <= max; i++) {
+    for (var i = Math.ceil(min); i <= max; i++) {
         if (i % 2 === 0) {
             sum += i;
         }
